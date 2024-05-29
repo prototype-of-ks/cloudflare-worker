531af98afc7db1288797dd146a5f6f32204e942f
prototype-of-ks <47179951+prototype-of-ks@users.noreply.github.com>
2024-05-29 16:24:47 +0800

    update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -2,8 +2,8 @@ import fs from "fs";
 
 try {
   fs.writeFileSync(
-    'dist/_headers.txt',
-    fs.readFileSync('_headers.txt').toString()
+    'dist/_headers',
+    fs.readFileSync('_headers').toString()
   );
 
   console.log('Header copied to "dist/_headers.txt"');
