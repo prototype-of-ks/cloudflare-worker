b204f429af98d662de3f7071bbedbec6f94df7d6
prototype-of-ks <47179951+prototype-of-ks@users.noreply.github.com>
2024-05-30 15:19:17 +0800

    remove require owner_role apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import zoomSdk, {
-  ConfigResponse,
-  RunningContext,
-  Participant,
-} from '@zoom/appssdk';
-import { useEffect, useState, useCallback } from 'react';
-import './App.css';
+import zoomSdk, { ConfigResponse, RunningContext } from "@zoom/appssdk";
+import { useEffect, useState, useCallback } from "react";
+import "./App.css";
 
 const App: React.FC = () => {
   const [config, setConfig] = useState<ConfigResponse>();
   const [runningContext, setRunningContext] = useState<RunningContext>();
-  const [participants, setParticipants] = useState<Participant[]>([]);
+  // const [participants, setParticipants] = useState<Participant[]>([]);
 
   const init = useCallback(async () => {
     const { context } = await zoomSdk.getRunningContext();
-    const { participants } = await zoomSdk.getMeetingParticipants();
+    // const { participants } = await zoomSdk.getMeetingParticipants();
 
-    setParticipants(participants);
+    // setParticipants(participants);
     setRunningContext(context);
   }, []);
 
@@ -32,16 +28,16 @@ const App: React.FC = () => {
     // await closeRenderingContext();
 
     const response = await zoomSdk.runRenderingContext({
-      view: 'immersive',
+      view: "immersive",
     });
-    console.log('runRenderingContext::camera => ', response);
+    console.log("runRenderingContext::camera => ", response);
   }, []);
 
   const drawWebview = useCallback(async () => {
     await runRenderingContext();
 
     const response = await zoomSdk.drawWebView({
-      webviewId: 'camera',
+      webviewId: "camera",
       x: 0,
       y: 0,
       width: config?.media?.renderTarget?.width,
@@ -51,12 +47,12 @@ const App: React.FC = () => {
 
     await zoomSdk.showNotification({
       // @ts-expect-error
-      title: 'Zoom SDK Notification',
-      type: 'info',
-      message: 'Would you like to join AI Companion?'
+      title: "Zoom SDK Notification",
+      type: "info",
+      message: "Would you like to join AI Companion?",
     });
 
-    console.log('drawWebview::camera => ', response);
+    console.log("drawWebview::camera => ", response);
   }, [runRenderingContext, config]);
 
   useEffect(() => {
@@ -64,26 +60,25 @@ const App: React.FC = () => {
     (async () => {
       const config = await zoomSdk.config({
         capabilities: [
-          'getRunningContext',
-          'getAppContext',
-          'clearImage',
-          'clearParticipant',
-          'closeRenderingContext',
-          'connect',
-          'drawImage',
-          'drawParticipant',
-          'getMeetingParticipants',
-          'getMeetingUUID',
-          'getRunningContext',
-          'getUserContext',
-          'onConnect',
-          'onMeeting',
-          'onMessage',
-          'onMyMediaChange',
-          'onParticipantChange',
-          'postMessage',
-          'runRenderingContext',
-          'sendAppInvitationToAllParticipants',
+          "getRunningContext",
+          "getAppContext",
+          "clearImage",
+          "clearParticipant",
+          "closeRenderingContext",
+          "connect",
+          "drawImage",
+          "drawParticipant",
+          "getMeetingUUID",
+          "getRunningContext",
+          "getUserContext",
+          "onConnect",
+          "onMeeting",
+          "onMessage",
+          "onMyMediaChange",
+          "onParticipantChange",
+          "postMessage",
+          "runRenderingContext",
+          "sendAppInvitationToAllParticipants",
         ],
       });
       setConfig(config);
@@ -91,29 +86,29 @@ const App: React.FC = () => {
       const context = await zoomSdk.getAppContext();
       await init();
 
-      console.log('context => ', context);
+      console.log("context => ", context);
     })();
   }, [config, init]);
 
   useEffect(() => {
-    if (config) console.log('config => ', config);
-    if (participants.length !== 0)
-      console.log('participants => ', participants);
-    if (runningContext) console.log('runningContext => ', runningContext);
-  }, [config, participants, runningContext]);
+    if (config) console.log("config => ", config);
+    // if (participants.length !== 0)
+    //   console.log('participants => ', participants);
+    if (runningContext) console.log("runningContext => ", runningContext);
+  }, [config, runningContext]);
 
   return (
     <>
-      {runningContext === 'inMeeting' && (
+      {runningContext === "inMeeting" && (
         <>
           <p className="read-the-docs">Zoom AI Notification</p>
           <button onClick={drawWebview}>Draw Webview</button>
           <button onClick={closeRenderingContext}>Close Webview</button>
         </>
       )}
-      {runningContext === 'inCamera' && (
+      {runningContext === "inCamera" && (
         <div className="glass">
-          <span className="name-tag">{participants[0].screenName}</span>
+          <span className="name-tag">Here is your screen Name</span>
         </div>
       )}
     </>
