f2254ea7773640567ba7aef9d3cfd072f53a596d
prototype-of-ks <47179951+prototype-of-ks@users.noreply.github.com>
2024-06-03 13:54:01 +0800

    May 31/feature: UserContext

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
-// 将所有 solid 图标添加到库中
-library.add(fas);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
