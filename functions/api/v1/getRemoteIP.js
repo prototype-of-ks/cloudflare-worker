1dbdb5f795e4564ea1696719f98906ab3e0bf947
prototype-of-ks <47179951+prototype-of-ks@users.noreply.github.com>
2024-06-05 15:09:55 +0800

    May 31/feature: getRemoteIP

--- functions/api/v1/getRemoteIP.js
+++ functions/api/v1/getRemoteIP.js
@@ -1,5 +1,5 @@
 export function onRequest(context) {
-  return new Response(JSON.stringify(context), {
+  return new Response(JSON.stringify(context.request), {
     headers: {
         'content-type': 'application/json;charset=UTF-8'
     }
